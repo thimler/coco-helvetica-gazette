fa3bbbea0ad53f1961a5bdce572048faa3541565
Antoine Albertelli <antoinealb@google.com>
2020-04-04 17:27:53 +0200

    Display author age and gender

--- django/pages/models.py
+++ django/pages/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+GENDERS = [
+    ('M', 'Homme'),
+    ('F', 'Femme'),
+    ('O', 'Autre'),
+]
+
 class Tag(models.Model):
     name = models.CharField(max_length=200)
 
@@ -10,6 +16,8 @@ class TextTestimonial(models.Model):
     text = models.TextField()
     creation_date = models.DateField(auto_now_add=True)
     tags = models.ManyToManyField(Tag)
+    author_age = models.IntegerField(blank=True, null=True)
+    author_gender = models.CharField(max_length=1, choices=GENDERS, null=True, blank=True)
 
     def __str__(self):
         return '"' + " ".join(self.text.split()[:5]) + '"'
