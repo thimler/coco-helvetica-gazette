3a53dbbe0e1361028af5dd375c5bdac06eb96607
Antoine Albertelli <antoinealb@google.com>
2020-04-04 15:35:06 +0200

    Basic support for users testimonials

--- django/pages/models.py
+++ django/pages/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class TextTestimonial(models.Model):
+    text = models.TextField()
+    creation_date = models.DateField(auto_now_add=True)
--- django/pages/views.py
+++ django/pages/views.py
@@ -1,5 +1,7 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from .models import TextTestimonial
 
 
-class HomePageView(TemplateView):
+class HomePageView(ListView):
     template_name = 'pages/home.html'
+    model = TextTestimonial
