679c0dc83e10ead77b16ea0b5e00e7fadf980fdd
Eric Dupertuis <contact@edupertuis.net>
2020-04-04 23:57:26 +0200

    Masonry look with filters (dummy)

--- django/pages/views.py
+++ django/pages/views.py
@@ -7,6 +7,11 @@ class HomePageView(ListView):
     template_name = "pages/home.html"
     model = TextTestimonial
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tags'] = Tag.objects.all()
+        return context
+
 
 class ArticleView(DetailView):
     model = TextTestimonial
