f6524ed2c64bf205cbd58b5623332ca4bd0da10f
Antoine Albertelli <antoinealb@google.com>
2020-04-04 16:44:02 +0200

    Add basic hashtag support

--- django/pages/admin.py
+++ django/pages/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import TextTestimonial
+from .models import TextTestimonial, Tag
 
 admin.site.register(TextTestimonial)
+admin.site.register(Tag)
--- django/pages/models.py
+++ django/pages/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return "#" + self.name
+
 class TextTestimonial(models.Model):
     text = models.TextField()
     creation_date = models.DateField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag)
